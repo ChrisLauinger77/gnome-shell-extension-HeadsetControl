32dd7133c2159979a681f1af51b8019fa1ece13a
Christian Lauinger <christian.lauinger@gmail.com>
2025-06-28 09:00:40 +0200

    Allows packing with argument "pack"
    Extends the install script to allow packing the extension
    zip file using the argument "pack". This provides an
    alternative way to create the zip file.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ gnome-extensions pack --podir=../po/ --out-dir=../ --extra-source=../LICENSE
 cd ..
 mv HeadsetControl@lauinger-clan.de.shell-extension.zip HeadsetControl@lauinger-clan.de.zip
 
-if [ "$1" = "zip" ]; then
+if [ "$1" = "zip" ] || [ "$1" = "pack" ]; then
    echo "Extension zip created ..."
 else
     gnome-extensions install HeadsetControl\@lauinger-clan.de.zip --force
