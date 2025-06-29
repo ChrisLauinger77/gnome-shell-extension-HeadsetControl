3bf2a195107ade49d51be0755719b3ed0b9bb6c8
Christian Lauinger <christian.lauinger@gmail.com>
2025-06-29 17:04:29 +0200

    Fixes extension installation and enabling
    Updates the install script to correctly install and enable the gnome extension.
    
    It uses the correct filename when installing and enabling the extension, which was previously incorrect, preventing the extension from being installed and enabled properly.

--- install.sh
+++ install.sh
@@ -3,14 +3,22 @@
 # glib-compile-schemas HeadsetControl\@lauinger-clan.de/schemas/
 
 cd HeadsetControl\@lauinger-clan.de
-gnome-extensions pack --podir=../po/ --out-dir=../ --extra-source=../LICENSE
+gnome-extensions pack --podir=../po/ --out-dir=../ --extra-source=../LICENSE --force
 cd ..
-mv HeadsetControl@lauinger-clan.de.shell-extension.zip HeadsetControl@lauinger-clan.de.zip
 
-if [ "$1" = "zip" ] || [ "$1" = "pack" ]; then
-   echo "Extension zip created ..."
-else
-    gnome-extensions install HeadsetControl\@lauinger-clan.de.zip --force
-    gnome-extensions enable HeadsetControl\@lauinger-clan.de
-fi
-
+case "$1" in
+  zip|pack)
+    echo "Extension zip created ..."
+    ;;
+  install)
+    gnome-extensions install HeadsetControl\@lauinger-clan.de.shell-extension.zip --force
+    gnome-extensions enable HeadsetControl\@lauinger-clan.shell-extension.de
+    ;;
+  upload)
+    gnome-extensions upload HeadsetControl\@lauinger-clan.de.shell-extension.zip
+    ;;
+  *)
+    echo "Usage: $0 {zip|pack|install|upload}"
+    exit 1
+    ;;
+esac
